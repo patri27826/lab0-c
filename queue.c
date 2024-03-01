897587761b425db527e435ba61838b9053d22c6b
Patrick Lin <patrick41513@gmail.com>
2024-03-01 17:25:13 +0800

    Implement insertion of head element in queue

--- queue.c
+++ queue.c
@@ -39,6 +39,17 @@ void q_free(struct list_head *head)
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
 {
+    if (!head || !s)
+        return false;
+    element_t *new = malloc(sizeof(element_t));
+    if (!new)
+        return false;
+    new->value = strdup(s);
+    if (!new->value) {
+        free(new);
+        return false;
+    }
+    list_add(&new->list, head);
     return true;
 }
 
